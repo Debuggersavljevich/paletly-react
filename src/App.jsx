4a4f31c93bdbbf8cebbae5c05cc00ab74969fa7b
Santiago Bogosavljevich <sbogosavljevich@gmail.com>
2022-07-23 23:12:27 -0300

    se agregan funcionalidades

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ function App() {
   return (
     <>
     
-    <h1 className='text-5xl font-bold text-white drop-shadow-2xl text-center mt-24 font-inter '>Paletly V2!😎</h1>
-    <h2 className='text-3xl font-bold text-white drop-shadow-2xl text-center mt-24 font-inter '>¡Crea tus paletas de colores, con un sólo click!🧉</h2>
+    <h1 className='text-5xl font-bold text-white drop-shadow-2xl text-center mt-24 font-inter '>Paletly! (Ahora en React)😎</h1>
+    <h2 className='text-3xl font-bold text-white drop-shadow-2xl text-center mt-12 font-inter '>¡Crea tus paletas de colores, con un sólo click!🧉</h2>
     <h3 className='text-xl font-bold text-white drop-shadow-2xl text-center mt-24 font-inter '>¡Para copiar el color hacé click en copiar!🥳</h3>
     
+
+
     <div className='flex justify-between'>
-      
+
       <Box/>
       <Box/>
       <Box/>
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -36,7 +36,10 @@ const Box = () => {
           }
       
           setColor(rgbToHex(color.r, color.g, color.b))}
-
+          
+         
+            const [disable, setDisable] = useState(false);
+           
 
 
        
@@ -44,13 +47,18 @@ const Box = () => {
 
   return (
     <>
-    <div onClick={()=>navigator.clipboard.writeText(color)} style={{ backgroundColor: color }} className='mt-28 ml-8 mr-8 h-72 w-72 bg-red-200 rounded-t-[16px] drop-shadow-xl transition-colors text-center cursor-pointer border-4 border-white'>
+    <div onClick={()=>navigator.clipboard.writeText(color)} style={{ backgroundColor: color }} className='mt-28 ml-8 mr-8 h-72 w-72 bg-red-200 rounded-t-[16px] drop-shadow-xl transition-colors text-center cursor-pointer border-2 border-white hover:transition-all hover:h-80 hover:w-80'>
                 
-        <button onClick={handleGenerate} className='bg-white rounded-md h-12 w-24 relative top-48  hover:bg-indigo-200 transition-colors'>Generar!</button>
+        <button disabled={disable} onClick={handleGenerate} className='bg-white rounded-md h-12 w-24 relative top-48  hover:bg-indigo-200 transition-colors'>Generar!</button>
         
+        
+
         <div>
             <span className='font-bold text-white'>{color}</span>
-            <button onClick={notify} className='ml-6 h-12 w-24 bg-white rounded-md'>copiar</button>
+            <button id='generator' onClick={notify} className='ml-6 h-12 w-24 bg-white rounded-md'>copiar</button>
+        </div>
+        <div className='mt-6 text-center'>
+          <button onClick={() => setDisable(true)}><svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 0 24 24" width="36px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 17c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm6-9h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6h1.9c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm0 12H6V10h12v10z"/></svg></button>
         </div>
     </div>
 
